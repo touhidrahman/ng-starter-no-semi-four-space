b4fc0de9cadd3c1f29105cbbdbd027eb55456342
Touhidur Rahman <touhidrahman1987@gmail.com>
2020-04-10 11:30:46 +0200

    Clean root component

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -6,8 +6,6 @@ import { Component, OnInit } from '@angular/core'
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-    title = 'ng-starter-no-semi-four-space'
-
     constructor() {}
 
     ngOnInit() {}
