3a8cac56b54a65c6541a713cf0bd478d45a37f78
Touhidur Rahman <touhidur.rahman@check24.de>
2020-11-18 18:14:01 +0100

    Upgrade to ng11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
-import { AppComponent } from './app.component'
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
     {
