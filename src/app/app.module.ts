4966d721a5378c0747a994bcebf0ecbaac6d9f64
Touhidur Rahman <touhidur.rahman@check24.de>
2021-02-13 22:00:45 +0100

    Include tailwind css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { RouterModule } from '@angular/router'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
 
 @NgModule({
     declarations: [AppComponent],
--- src/app/features/comments/services/comment.service.ts
+++ src/app/features/comments/services/comment.service.ts
@@ -1,9 +1,9 @@
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Params } from '@angular/router';
-import { BaseService } from '@core/services/base.service';
-import { Comment, CommentDto } from '@features/comments/interfaces/comment';
+import { Observable } from 'rxjs'
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Params } from '@angular/router'
+import { BaseService } from '@core/services/base.service'
+import { Comment, CommentDto } from '@features/comments/interfaces/comment'
 
 @Injectable({
     providedIn: 'root',
