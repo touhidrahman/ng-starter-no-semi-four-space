a0a156ae4f88cd5761293cb6c01de8fbd154098d
Touhidur Rahman <touhidur.rahman@check24.de>
2021-03-21 16:38:26 +0100

    Add ng-zorro-antd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,19 @@ import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { RouterModule } from '@angular/router'
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms'
+
+registerLocaleData(en);
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserAnimationsModule, BrowserModule, RouterModule, HttpClientModule, AppRoutingModule],
-    providers: [],
+    imports: [BrowserAnimationsModule, BrowserModule, RouterModule, HttpClientModule, AppRoutingModule, FormsModule],
+    providers: [{ provide: NZ_I18N, useValue: en_US }],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -4,9 +4,10 @@ import { ArticlesModule } from '@features/articles/articles.module'
 import { CommentsModule } from '@features/comments/comments.module'
 import { HomePageRoutingModule } from './home-page-routing.module'
 import { HomePageComponent } from './home-page.component'
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @NgModule({
     declarations: [HomePageComponent],
-    imports: [CommonModule, ArticlesModule, CommentsModule, HomePageRoutingModule],
+    imports: [CommonModule, ArticlesModule, CommentsModule, NzButtonModule, HomePageRoutingModule],
 })
 export class HomePageModule {}
