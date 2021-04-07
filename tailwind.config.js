c960fc34f49a93f6e4dab42d7ace5f2cee5ac34b
Touhidur Rahman <touhidur.rahman@check24.de>
2021-04-07 19:46:14 +0200

    Use ngneat for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
+const { guessProductionMode } = require("@ngneat/tailwind");
+
+process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
+console.log('TAILWIND_MODE ', process.env.TAILWIND_MODE)
+
 module.exports = {
     prefix: '',
     mode: 'jit',
     purge: {
-        content: [
-            './src/**/*.{html,ts}',
-        ]
+      content: [
+        './src/**/*.{html,ts,css,scss,sass,less,styl}',
+      ]
     },
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+      extend: {},
     },
     variants: {
-        extend: {},
+      extend: {},
     },
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography')
-    ],
+    plugins: [require('@tailwindcss/aspect-ratio'),require('@tailwindcss/forms'),require('@tailwindcss/line-clamp')],
 };
