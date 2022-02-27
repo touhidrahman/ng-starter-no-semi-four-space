1148643d9f290aa80fa09046e8065f45564836f5
Touhidur Rahman <touhidur.rahman@check24.de>
2022-02-27 21:01:23 +0100

    Add bytes pipe

--- src/app/core/pipes/pipes.module.ts
+++ src/app/core/pipes/pipes.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { SpreadIntoArrayPipe } from '@core/pipes/spread-into-array.pipe'
+import { BytesPipe } from './bytes.pipe'
 import { CeilingPipe } from './ceiling.pipe'
 import { Nl2brPipe } from './nl2br.pipe'
 import { SafeUrlPipe } from './safe-url.pipe'
@@ -8,8 +9,8 @@ import { UniquePipe } from './unique.pipe'
 import { ValueInPipe } from './value-in.pipe'
 
 @NgModule({
-    declarations: [SafeUrlPipe, SpreadIntoArrayPipe, CeilingPipe, Nl2brPipe, UniquePipe, ValueInPipe],
-    exports: [SafeUrlPipe, SpreadIntoArrayPipe, CeilingPipe, Nl2brPipe, UniquePipe, ValueInPipe],
+    declarations: [SafeUrlPipe, SpreadIntoArrayPipe, CeilingPipe, Nl2brPipe, UniquePipe, ValueInPipe, BytesPipe],
+    exports: [SafeUrlPipe, SpreadIntoArrayPipe, CeilingPipe, Nl2brPipe, UniquePipe, ValueInPipe, BytesPipe],
     imports: [CommonModule],
 })
 export class PipesModule {}
