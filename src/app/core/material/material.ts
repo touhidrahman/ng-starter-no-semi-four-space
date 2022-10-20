6e5358e61cb4368d206647c06f01fa9fd8334cee
Touhid Rahman <touhidrahman1987@gmail.com>
2022-10-20 06:03:12 -0400

    Add all material things in export

--- src/app/core/material/material.ts
+++ src/app/core/material/material.ts
@@ -1,3 +1,15 @@
+import { A11yModule } from '@angular/cdk/a11y'
+import { CdkAccordionModule } from '@angular/cdk/accordion'
+import { ClipboardModule } from '@angular/cdk/clipboard'
+import { DialogModule } from '@angular/cdk/dialog'
+import { DragDropModule } from '@angular/cdk/drag-drop'
+import { CdkMenuModule } from '@angular/cdk/menu'
+import { OverlayModule } from '@angular/cdk/overlay'
+import { PortalModule } from '@angular/cdk/portal'
+import { ScrollingModule } from '@angular/cdk/scrolling'
+import { CdkStepperModule } from '@angular/cdk/stepper'
+import { CdkTableModule } from '@angular/cdk/table'
+import { CdkTreeModule } from '@angular/cdk/tree'
 import { MatAutocompleteModule } from '@angular/material/autocomplete'
 import { MatBadgeModule } from '@angular/material/badge'
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet'
@@ -12,10 +24,13 @@ import { MatDialogModule } from '@angular/material/dialog'
 import { MatDividerModule } from '@angular/material/divider'
 import { MatExpansionModule } from '@angular/material/expansion'
 import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatGridListModule } from '@angular/material/grid-list'
 import { MatIconModule } from '@angular/material/icon'
 import { MatInputModule } from '@angular/material/input'
 import { MatListModule } from '@angular/material/list'
 import { MatMenuModule } from '@angular/material/menu'
+import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatProgressBarModule } from '@angular/material/progress-bar'
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 import { MatRadioModule } from '@angular/material/radio'
 import { MatSelectModule } from '@angular/material/select'
@@ -23,12 +38,25 @@ import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 import { MatSliderModule } from '@angular/material/slider'
 import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { MatSortModule } from '@angular/material/sort'
+import { MatStepperModule } from '@angular/material/stepper'
 import { MatTableModule } from '@angular/material/table'
 import { MatTabsModule } from '@angular/material/tabs'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatTooltipModule } from '@angular/material/tooltip'
+import { MatTreeModule } from '@angular/material/tree'
 
 export const MaterialModules = [
+    A11yModule,
+    CdkAccordionModule,
+    CdkMenuModule,
+    CdkStepperModule,
+    CdkTableModule,
+    CdkTreeModule,
+    ClipboardModule,
+    DialogModule,
+    DragDropModule,
+    MatAutocompleteModule,
     MatAutocompleteModule,
     MatBadgeModule,
     MatBottomSheetModule,
@@ -43,12 +71,14 @@ export const MaterialModules = [
     MatDividerModule,
     MatExpansionModule,
     MatFormFieldModule,
+    MatGridListModule,
     MatIconModule,
     MatInputModule,
     MatListModule,
     MatMenuModule,
-    MatMenuModule,
     MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
     MatProgressSpinnerModule,
     MatRadioModule,
     MatRippleModule,
@@ -57,8 +87,14 @@ export const MaterialModules = [
     MatSlideToggleModule,
     MatSliderModule,
     MatSnackBarModule,
+    MatSortModule,
+    MatStepperModule,
     MatTableModule,
     MatTabsModule,
     MatToolbarModule,
     MatTooltipModule,
+    MatTreeModule,
+    OverlayModule,
+    PortalModule,
+    ScrollingModule,
 ]
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,8 @@ module.exports = {
         extend: {
             colors: {
                 // material light blue pallete
-                'primary': {
-                     50: '#e1f5fe',
+                primary: {
+                    50: '#e1f5fe',
                     100: '#b3e5fc',
                     200: '#81d4fa',
                     300: '#4fc3f7',
@@ -17,15 +17,15 @@ module.exports = {
                     600: '#039be5',
                     700: '#0288d1',
                     800: '#0277bd',
-                    900: '#01579b'
+                    900: '#01579b',
                 },
                 'primary-hue-100': '#80D8FF',
                 'primary-hue-200': '#40C4FF',
                 'primary-hue-400': '#00B0FF',
                 'primary-hue-700': '#0091EA',
                 // material teal pallete
-                'accent': {
-                     50: '#e0f2f1',
+                accent: {
+                    50: '#e0f2f1',
                     100: '#b2dfdb',
                     200: '#80cbc4',
                     300: '#4db6ac',
@@ -34,15 +34,15 @@ module.exports = {
                     600: '#00897b',
                     700: '#00796b',
                     800: '#00695c',
-                    900: '#004d40'
+                    900: '#004d40',
                 },
                 'accent-hue-100': '#A7FFEB',
                 'accent-hue-200': '#64FFDA',
                 'accent-hue-400': '#1DE9B6',
                 'accent-hue-700': '#00BFA5',
                 // material red pallete
-                'warn': {
-                     50: '#ffebee',
+                warn: {
+                    50: '#ffebee',
                     100: '#ffcdd2',
                     200: '#ef9a9a',
                     300: '#e57373',
@@ -56,7 +56,7 @@ module.exports = {
                 'warn-hue-100': '#FF8A80',
                 'warn-hue-200': '#FF5252',
                 'warn-hue-400': '#FF1744',
-                'warn-hue-700': '#D50000'
+                'warn-hue-700': '#D50000',
             },
         },
     },
@@ -69,5 +69,6 @@ module.exports = {
         require('@tailwindcss/typography'),
         require('@tailwindcss/line-clamp'),
         require('tailwind-scrollbar'),
+        require('prettier-plugin-tailwindcss'),
     ],
 }
