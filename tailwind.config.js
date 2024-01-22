2d976568b555f6bc081334d3191959328588abee
Touhid Rahman <touhidrahman1987@gmail.com>
2024-01-22 07:11:37 -0500

    Fix spartan issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,8 @@
 /* eslint-disable no-undef */
 module.exports = {
     prefix: '',
-    content: [
-        './src/**/*.{html,ts,css,scss,sass,less,styl}',
-        './libs/ui/**/*.{html,ts}',
-    ],
+    presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
+    content: ['./src/**/*.{html,ts,css,scss,sass,less,styl}', './libs/ui/**/*.{html,ts}'],
     darkMode: 'class', // or 'media'
     theme: {
         extend: {},
