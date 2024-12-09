b62cd31e8f23a80e368589fbc65d5e73dc822da7
Touhid Rahman <touhidrahman1987@gmail.com>
2024-12-08 21:00:09 -0500

    Update make-env script

--- src/environments/make-env-file.ts
+++ src/environments/make-env-file.ts
@@ -1,11 +1,14 @@
-import { writeFile } from 'node:fs'
+import { readFile, writeFile } from 'node:fs'
+import path from 'node:path'
 import { AppEnvironment } from './app-environment.interface'
 
 /**
  * This script makes a prod environment file in angular project's environment dir
  */
 
-const filename = 'src/environments/environment.prod.ts'
+const directoryPath = './src/environments'
+const fileName = 'environment.prod.ts'
+const filePath = path.join(directoryPath, fileName)
 
 function getAppEnvironment(): AppEnvironment {
     return {
@@ -19,10 +22,23 @@ function getContent(): string {
     return `export const environment = ${JSON.stringify(getAppEnvironment(), null, 4)}`
 }
 
-writeFile(filename, getContent(), function onCreate(err: unknown) {
-    if (err) throw err
-    console.warn('Running ./make-env-file.ts script to create angular environment file')
-    console.info('#################### MAKE-ENV-FILE ####################')
-    console.info(`${filename} created`)
-    console.info('#######################################################')
+// Read the file
+readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading the file:', err.message)
+    } else {
+        console.warn('Running ./make-env-file.ts script to create angular environment file')
+        console.info('Existing File content:\n', data)
+    }
 })
+
+setTimeout(() => {
+    writeFile(filePath, getContent(), function onCreate(err: unknown) {
+        if (err) throw err
+        console.info('#################### MAKE-ENV-FILE ####################')
+        console.info(`${filePath} created`)
+        console.info('File content:')
+        console.info(getContent())
+        console.info('#######################################################')
+    })
+}, 100)
